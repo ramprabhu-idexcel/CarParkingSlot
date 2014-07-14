a60ffa024f5338a6abe765617680911dd64e3154
Ramprabhu <ramprabu.narayanasamy@idexcel.com>
2014-07-14 12:33:34 +0530

    changes done in ruby program.rb file

--- program.rb
+++ program.rb
@@ -4,11 +4,11 @@ if ARGV.length == 1
    Car::Input.read(ARGV[0])
 else
    car_obj = Car::Search.new
-   puts "Enter the input:"
    200.times do
+     puts "Please enter the input:"
      user_input = STDIN.gets.chomp
      car_obj.store_find_detail(user_input)
-     puts "Do you want to continue? type yes/no"
+     print "Do you want to continue? type yes/no: "
      name=gets.chomp
      break if name=="no"
    end
